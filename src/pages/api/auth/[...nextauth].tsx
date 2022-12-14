995ab5ae4b6ae2550c5a00e1990978e9e6a6a98c
Dawid Kiełkowski <dawid55a@gmail.com>
2022-12-14 18:59:45 +0100

    API requests

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -6,8 +6,15 @@ export default NextAuth({
     StravaProvider({
       clientId: '97926',
       clientSecret: 'b311304ac9e469ccc742d359d6c9ddbeede1dd01',
+      authorization: {
+        params: {
+          scope:
+            'read,read_all,profile:read_all,activity:read,activity:read_all',
+        },
+      },
     }),
   ],
+
   pages: {
     signIn: '/signin',
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Button } from 'flowbite-react';
 import type { NextPage } from 'next';
 import { getCsrfToken, signIn, signOut, useSession } from 'next-auth/react';
@@ -5,15 +6,60 @@ import { useEffect } from 'react';
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
+  let csrfToken: string | undefined;
 
-  async function myFunction() {
-    const csrfToken = await getCsrfToken();
-    console.log(csrfToken);
-  }
   useEffect(() => {
     console.log(session);
-    myFunction();
+
   });
+
+  async function showData() {
+    // csrfToken = await getCsrfToken();
+    csrfToken = 'f6134e9907c0c74b07dfb8b7673697be09339085';
+    console.log(csrfToken)
+    const synch = await axios
+      .get('http://localhost:8080/api/v1/synchronize', {
+        headers: {
+          'Authorization': `Bearer ${csrfToken}`,
+          'Accept-Encoding': 'identity',
+        },
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(synch);
+
+    const data = await axios
+      .get('http://localhost:8080/api/v1/athlete', {
+        headers: {
+          Authorization: `Bearer ${csrfToken}`,
+        },
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    const datastrava = await axios
+      .get('https://www.strava.com/api/v3/athlete', {
+        headers: {
+          Authorization: `Bearer ${csrfToken}`,
+        },
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(datastrava);
+  }
+
   return (
     <>
       <div className="">
@@ -23,6 +69,11 @@ const Home: NextPage = () => {
         )}
         {session && (
           <Button onClick={() => signOut()}>Disconnect from Strava</Button>
+        )}
+        {session && (
+          <Button color={'purple'} onClick={() => showData()}>
+            show data
+          </Button>
         )}
       </div>
     </>
