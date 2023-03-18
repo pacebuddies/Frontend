760ebab19ee90f98f69528829877c74985a66b0b
Dawid Kiełkowski <dawid55a@gmail.com>
2023-03-18 15:59:58 +0100

    Added store for settings

--- src/components/Settings/AccountSettingTab.tsx
+++ src/components/Settings/AccountSettingTab.tsx
@@ -1,7 +1,33 @@
+import { Dropdown } from 'flowbite-react';
 import { NextPage } from 'next';
+import {
+  useSetSettingsStore,
+  useSettingsStore,
+} from '../../store/settingsStore';
 
 const AccountSettingTab: NextPage = () => {
-  return <div>Account settings panel</div>;
+  const settings = useSettingsStore((state) => state.settings);
+  const setSettings = useSetSettingsStore((state) => state.setSettings);
+
+  const handleUnitsChange = (value: 'metric' | 'imperial') => {
+    setSettings({ settings: { measurementUnits: value } });
+  };
+
+  return (
+    <div>
+      Account settings panel
+      <div>
+        <Dropdown label={settings.measurementUnits}>
+          <Dropdown.Item onClick={() => handleUnitsChange('metric')}>
+            Metric
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleUnitsChange('imperial')}>
+            Imperial
+          </Dropdown.Item>
+        </Dropdown>
+      </div>
+    </div>
+  );
 };
 
 export default AccountSettingTab;
