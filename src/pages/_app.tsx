f563bee948e26b227c698b0741ff0f982862531d
Dawid Kiełkowski <dawid55a@gmail.com>
2023-04-10 21:36:56 +0200

    Added react-query package with devtools and recreated yarn.lock

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import { QueryClient } from '@tanstack/query-core';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { CustomFlowbiteTheme, Flowbite } from 'flowbite-react';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
@@ -5,11 +8,16 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Flowbite theme={{ theme }}>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </Flowbite>
       <ToastContainer
         position="bottom-right"
