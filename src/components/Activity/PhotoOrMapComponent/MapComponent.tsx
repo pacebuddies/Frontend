f5dfc10e3fd691f8abcb6ef2cd07ac4f27a30809
Dawid Kiełkowski <dawid55a@gmail.com>
2023-05-19 23:27:17 +0200

    Update MapComponent, removed zoom on scrolling

--- src/components/Activity/PhotoOrMapComponent/MapComponent.tsx
+++ src/components/Activity/PhotoOrMapComponent/MapComponent.tsx
@@ -18,7 +18,7 @@ const MapComponent = ({ activity, pathOptions }: IMapComponentProps) => {
     <MapContainer
       center={startingPoint}
       zoom={13}
-      scrollWheelZoom={true}
+      scrollWheelZoom={false}
       style={{ height: '100%', width: '100%' }}
     >
       <TileLayer
